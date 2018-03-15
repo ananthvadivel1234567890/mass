408cace2e6631625764d5939697b85c71f685a56
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-03-15 16:51:52 +0530

    Create m.c

---
+++ m.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int h,m,h1,m1,h2,m2;
+printf("enter 2 times in hour and minute format");
+scanf("%d%d",&h1,&m1);
+scanf("%d%d",&h2,&m2);
+h=h2-h1;
+m=m2-m1;
+printf("%d\t%d",h,m);
+}
